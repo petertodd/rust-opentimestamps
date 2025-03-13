49fb3ec4b22821a0bc14ae5e1db5e17ea541a4b2
Peter Todd <pete@petertodd.org>
2025-03-13 15:15:03 +0000

    Improve docs

--- src/timestamp/detached.rs
+++ src/timestamp/detached.rs
@@ -1,5 +1,14 @@
+//! Detached timestamp proof files.
+//!
+//! This module contains support for the `.ots` files produced by the OpenTimestamps Client.
+
 use super::*;
 
+/// A digest of a file, used by `DetachedTimestampFile`.
+///
+/// Since a file may be of any size, a timestamp proof for a file always starts by hashing the
+/// entire file with one of the supported hashing algorithms. This type holds the expected digest,
+/// and the type of hashing algorithm to use.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum FileDigest {
     Sha1([u8; 20]),
@@ -27,6 +36,10 @@ impl AsRef<[u8]> for FileDigest {
     }
 }
 
+/// A timestamp proof for a file.
+///
+/// This is a wrapper for a `Timestamp<FileDigest>`, along with serialization/deserialization code
+/// for the timestamp file specific header.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct DetachedTimestampFile {
     inner: Timestamp<FileDigest>,
@@ -41,9 +54,30 @@ impl std::ops::Deref for DetachedTimestampFile {
 }
 
 impl DetachedTimestampFile {
-    const HEADER_MAGIC: &[u8; 31] = b"\x00OpenTimestamps\x00\x00Proof\x00\xbf\x89\xe2\xe8\x84\xe8\x92\x94";
+    /// The magic bytes that detached timestamp files start with.
+    ///
+    /// The magic bytes start with a null byte to make them clearly recognizable as a binary file,
+    /// and include the strings "OpenTimestamps" and "Proof" to make them easily recognized in hex
+    /// dumps:
+    ///
+    /// ```no_test
+    /// $ hexdump -C example.txt.ots
+    /// 00000000  00 4f 70 65 6e 54 69 6d  65 73 74 61 6d 70 73 00  |.OpenTimestamps.|
+    /// 00000010  00 50 72 6f 6f 66 00 bf  89 e2 e8 84 e8 92 94 01  |.Proof..........|
+    /// ```
+    ///
+    /// The remaining random bytes are simply randomly chosen non-ASCII bytes.
+    pub const HEADER_MAGIC: &[u8; 31] = b"\x00OpenTimestamps\x00\x00Proof\x00\xbf\x89\xe2\xe8\x84\xe8\x92\x94";
     const MAJOR_VERSION: u8 = 1;
 
+    /// The IANA Media Type that should be used for detached timestamp files.
+    ///
+    /// # References
+    ///
+    /// [IANA Media Type Assignment](https://www.iana.org/assignments/media-types/application/vnd.opentimestamps.ots)
+    pub const MEDIA_TYPE_STR: &str = "application/vnd.opentimestamps.ots";
+
+    /// Returns the `FileDigest`.
     pub fn digest(&self) -> &FileDigest {
         self.inner.msg()
     }
